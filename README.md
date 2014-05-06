165ba3c65f8a2c4d6ec87c4e6e01ba267184f879
yihua.huang <code4crafter@gmail.com>
2014-05-06 12:00:39 +0800

    code style

--- README.md
+++ README.md
@@ -7,14 +7,14 @@ Practice in OOP for thoughtworks quiz [FizzBuzzWhizz](https://www.jinshuju.net/f
 NumberSequenceSayer numberSequenceSayer = NumberSequenceSayerBuilder.custom()
    .setNumberSayer(
        or(contains(3).thenReturn("Fizz"))
-           .or(
-               concat(
-                  mod(3).is(0).thenReturn("Fizz"),
-                  mod(5).is(0).thenReturn("Buzz"),
-                  mod(7).is(0).thenReturn("Whizz")
-               )
+       .or(
+           concat(
+              mod(3).is(0).thenReturn("Fizz"),
+              mod(5).is(0).thenReturn("Buzz"),
+              mod(7).is(0).thenReturn("Whizz")
            )
-           .or(echoInputNumber())
+       )
+       .or(echoInputNumber())
    )
    .setStartNumber(1).setEndNumber(100).get();
 System.out.println(numberSequenceSayer.say());
--- src/test/java/us/codecraft/fizzBuzz/NumberSequenceSayerTest.java
+++ src/test/java/us/codecraft/fizzBuzz/NumberSequenceSayerTest.java
@@ -15,14 +15,14 @@ public class NumberSequenceSayerTest {
         NumberSequenceSayer numberSequenceSayer = NumberSequenceSayerBuilder.custom()
            .setNumberSayer(
                or(contains(3).thenReturn("Fizz"))
-                   .or(
-                       concat(
-                          mod(3).is(0).thenReturn("Fizz"),
-                          mod(5).is(0).thenReturn("Buzz"),
-                          mod(7).is(0).thenReturn("Whizz")
-                       )
+               .or(
+                   concat(
+                      mod(3).is(0).thenReturn("Fizz"),
+                      mod(5).is(0).thenReturn("Buzz"),
+                      mod(7).is(0).thenReturn("Whizz")
                    )
-                   .or(echoInputNumber())
+               )
+               .or(echoInputNumber())
            )
            .setStartNumber(1).setEndNumber(100).get();
         System.out.println(numberSequenceSayer.say());
